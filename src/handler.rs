9994d9c006c916556db54e51d3721a92cd3a118b
Saber Haj Rabiee <saber.hajrabiee@gmail.com>
2023-06-07 13:51:08 -0700

    fixed first datagram packet echoing on accept and added tcp and udp over tcp protocols

--- src/handler.rs
+++ src/handler.rs
@@ -1,15 +1,17 @@
 use crate::args::{Args, Commands};
 use crate::encrypt_stream::EncryptStream;
-use opool::PoolAllocator;
 use std::{net::SocketAddr, sync::Arc};
-use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
 const POOL_SIZE: usize = 1024 * 16;
 
+type PoolAllocator = opool::Pool<ObjectPoolAllocator, Vec<u8>>;
+type PoolAllocatorObject = opool::RcGuard<ObjectPoolAllocator, Vec<u8>>;
+
 struct ObjectPoolAllocator(usize);
 
-impl PoolAllocator<Vec<u8>> for ObjectPoolAllocator {
+impl opool::PoolAllocator<Vec<u8>> for ObjectPoolAllocator {
     #[inline]
     fn allocate(&self) -> Vec<u8> {
         vec![0; self.0]
@@ -45,13 +47,15 @@ async fn udp_listen(
     };
     let args = Arc::new(args);
     loop {
-        let (peer_sock, peer_addr) = bind_sock.accept(pool.get().as_mut()).await?;
+        let mut buf = pool.clone().get_rc();
+        let (n, peer_sock, peer_addr) = bind_sock.accept(buf.as_mut()).await?;
         tokio::spawn(new_peer(
             peer_addr,
             local_bind_ip,
             args.clone(),
             peer_sock,
             pool.clone(),
+            Some((buf, n)),
         ));
     }
 }
@@ -75,14 +79,12 @@ async fn tcp_listen(
             args.clone(),
             peer_sock,
             pool.clone(),
+            None,
         ));
     }
 }
 
-async fn uot_listen(
-    pool: Arc<opool::Pool<ObjectPoolAllocator, Vec<u8>>>,
-    args: Args,
-) -> std::io::Result<()> {
+async fn uot_listen(pool: Arc<PoolAllocator>, args: Args) -> std::io::Result<()> {
     let local_bind_ip: SocketAddr = match args.remote {
         SocketAddr::V4(_) => "0.0.0.0:0".parse().unwrap(),
         SocketAddr::V6(_) => "[::]:0".parse().unwrap(),
@@ -94,13 +96,15 @@ async fn uot_listen(
         Commands::Client => {
             let bind_sock = udpflow::UdpListener::new(args.listen)?;
             loop {
-                let (peer_sock, peer_addr) = bind_sock.accept(pool.get().as_mut()).await?;
+                let mut buf = pool.clone().get_rc();
+                let (n, peer_sock, peer_addr) = bind_sock.accept(buf.as_mut()).await?;
                 tokio::spawn(new_peer(
                     peer_addr,
                     local_bind_ip,
                     args.clone(),
                     peer_sock,
                     pool.clone(),
+                    Some((buf, n)),
                 ));
             }
         }
@@ -108,13 +112,13 @@ async fn uot_listen(
             let bind_sock = TcpListener::bind(args.listen).await?;
             loop {
                 let (peer_sock, peer_addr) = bind_sock.accept().await?;
-                let peer_sock = udpflow::UotStream::new(peer_sock);
                 tokio::spawn(new_peer(
                     peer_addr,
                     local_bind_ip,
                     args.clone(),
                     peer_sock,
                     pool.clone(),
+                    None,
                 ));
             }
         }
@@ -126,15 +130,16 @@ async fn new_peer<T>(
     local_bind_ip: SocketAddr,
     args: Arc<Args>,
     peer_sock: T,
-    pool: Arc<opool::Pool<ObjectPoolAllocator, Vec<u8>>>,
+    pool: Arc<PoolAllocator>,
+    buf: Option<(PoolAllocatorObject, usize)>,
 ) where
     T: AsyncRead + AsyncWrite + Unpin,
 {
     tracing::info!("new peer: {peer_addr}");
     let result = match args.protocol.as_str() {
-        "uot" => new_peer_uot(peer_addr, local_bind_ip, args, peer_sock, pool).await,
-        "tcp" => new_peer_tcp(peer_addr, args, peer_sock, pool).await,
-        "udp" => new_peer_udp(peer_addr, local_bind_ip, args, peer_sock, pool).await,
+        "uot" => new_peer_uot(peer_addr, local_bind_ip, args, peer_sock, pool, buf).await,
+        "tcp" => new_peer_tcp(peer_addr, args, peer_sock, pool, buf).await,
+        "udp" => new_peer_udp(peer_addr, local_bind_ip, args, peer_sock, pool, buf).await,
         _ => unreachable!(),
     };
     if let Err(err) = result {
@@ -147,13 +152,14 @@ async fn new_peer_udp<T>(
     local_bind_ip: SocketAddr,
     args: Arc<Args>,
     peer_sock: T,
-    pool: Arc<opool::Pool<ObjectPoolAllocator, Vec<u8>>>,
+    pool: Arc<PoolAllocator>,
+    buf: Option<(PoolAllocatorObject, usize)>,
 ) -> std::io::Result<()>
 where
     T: AsyncRead + AsyncWrite + Unpin,
 {
     let remote_sock = udpflow::UdpStreamRemote::new(local_bind_ip, args.remote).await?;
-    handle_client_server(peer_addr, args, peer_sock, remote_sock, pool).await;
+    encrypt(peer_addr, args, peer_sock, remote_sock, pool, buf).await;
     Ok(())
 }
 
@@ -161,13 +167,14 @@ async fn new_peer_tcp<T>(
     peer_addr: SocketAddr,
     args: Arc<Args>,
     peer_sock: T,
-    pool: Arc<opool::Pool<ObjectPoolAllocator, Vec<u8>>>,
+    pool: Arc<PoolAllocator>,
+    buf: Option<(PoolAllocatorObject, usize)>,
 ) -> std::io::Result<()>
 where
     T: AsyncRead + AsyncWrite + Unpin,
 {
     let remote_sock = TcpStream::connect(args.remote).await?;
-    handle_client_server(peer_addr, args, peer_sock, remote_sock, pool).await;
+    encrypt(peer_addr, args, peer_sock, remote_sock, pool, buf).await;
     Ok(())
 }
 
@@ -176,7 +183,8 @@ async fn new_peer_uot<T>(
     local_bind_ip: SocketAddr,
     args: Arc<Args>,
     peer_sock: T,
-    pool: Arc<opool::Pool<ObjectPoolAllocator, Vec<u8>>>,
+    pool: Arc<PoolAllocator>,
+    buf: Option<(PoolAllocatorObject, usize)>,
 ) -> std::io::Result<()>
 where
     T: AsyncRead + AsyncWrite + Unpin,
@@ -185,72 +193,79 @@ where
         Commands::Client => {
             let remote_sock = TcpStream::connect(args.remote).await?;
             let remote_sock = udpflow::UotStream::new(remote_sock);
-            handle_client_server(peer_addr, args, peer_sock, remote_sock, pool).await;
+            encrypt(peer_addr, args, peer_sock, remote_sock, pool, buf).await;
         }
         Commands::Server => {
+            let peer_sock = udpflow::UotStream::new(peer_sock);
             let remote_sock = udpflow::UdpStreamRemote::new(local_bind_ip, args.remote).await?;
-            handle_client_server(peer_addr, args, peer_sock, remote_sock, pool).await;
+            encrypt(peer_addr, args, peer_sock, remote_sock, pool, buf).await;
         }
     }
     Ok(())
 }
 
-async fn handle_client_server<T, U>(
-    peer_addr: SocketAddr,
-    args: Arc<Args>,
-    peer_sock: T,
-    remote_sock: U,
-    pool: Arc<opool::Pool<ObjectPoolAllocator, Vec<u8>>>,
-) where
-    T: AsyncRead + AsyncWrite + Unpin,
-    U: AsyncRead + AsyncWrite + Unpin,
-{
-    match args.command {
-        Commands::Client => {
-            encrypt(peer_addr, peer_sock, remote_sock, args, pool.clone()).await;
-        }
-        Commands::Server => {
-            encrypt(peer_addr, remote_sock, peer_sock, args, pool.clone()).await;
-        }
-    }
-}
-
 async fn encrypt<T, U>(
     peer_addr: SocketAddr,
+    args: Arc<Args>,
     peer_sock: T,
     remote_sock: U,
-    args: Arc<Args>,
-    pool: Arc<opool::Pool<ObjectPoolAllocator, Vec<u8>>>,
+    pool: Arc<PoolAllocator>,
+    buf: Option<(PoolAllocatorObject, usize)>,
 ) where
     T: AsyncRead + AsyncWrite + Unpin,
     U: AsyncRead + AsyncWrite + Unpin,
 {
     if let Some(encryption) = &args.encryption {
-        let peer_sock = EncryptStream::new(
-            peer_sock,
-            encryption.clone(),
-            crate::encrypt_stream::Mode::Encrypt,
-        );
-        let remote_sock = EncryptStream::new(
-            remote_sock,
-            encryption.clone(),
-            crate::encrypt_stream::Mode::Decrypt,
-        );
-        relay(peer_addr, peer_sock, remote_sock, args, pool).await;
+        match args.command {
+            Commands::Client => {
+                let peer_sock = EncryptStream::new(
+                    peer_sock,
+                    encryption.clone(),
+                    crate::encrypt_stream::Mode::Encrypt,
+                );
+                let remote_sock = EncryptStream::new(
+                    remote_sock,
+                    encryption.clone(),
+                    crate::encrypt_stream::Mode::Decrypt,
+                );
+                relay(peer_addr, peer_sock, remote_sock, args, pool, buf).await;
+            }
+            Commands::Server => {
+                let peer_sock = EncryptStream::new(
+                    peer_sock,
+                    encryption.clone(),
+                    crate::encrypt_stream::Mode::Decrypt,
+                );
+                let remote_sock = EncryptStream::new(
+                    remote_sock,
+                    encryption.clone(),
+                    crate::encrypt_stream::Mode::Encrypt,
+                );
+                relay(peer_addr, peer_sock, remote_sock, args, pool, buf).await;
+            }
+        }
         return;
     }
-    relay(peer_addr, peer_sock, remote_sock, args, pool).await;
+    relay(peer_addr, peer_sock, remote_sock, args, pool, buf).await;
 }
+
 async fn relay<T, U>(
     peer_addr: SocketAddr,
     mut peer_sock: T,
     mut remote_sock: U,
     args: Arc<Args>,
-    pool: Arc<opool::Pool<ObjectPoolAllocator, Vec<u8>>>,
+    pool: Arc<PoolAllocator>,
+    buf: Option<(PoolAllocatorObject, usize)>,
 ) where
     T: AsyncRead + AsyncWrite + Unpin,
     U: AsyncRead + AsyncWrite + Unpin,
 {
+    if let Some(buf) = buf {
+        if let Err(err) = remote_sock.write_all(&buf.0.as_ref()[..buf.1]).await {
+            tracing::error!("peer {peer_addr} failed sending first packet to remote: {err}");
+            return;
+        }
+    }
     let duration = match args.deadline {
         Some(deadline) => std::time::Duration::from_secs(deadline),
         None => std::time::Duration::from_secs(84600 * 365),
